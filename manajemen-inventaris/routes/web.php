b88e5e6ba7c17171744005acac367f3582d9ab65
Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
2026-01-17 20:54:37 +0700

    menambahkan navigation dan route untuk bagian index sekaligus membuat sebuah controller inventaris

--- manajemen-inventaris/routes/web.php
+++ manajemen-inventaris/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\inventarisController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [inventarisController::class, 'index']);
