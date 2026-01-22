07cf42dc7c74c168e65e1fec8fad4aefade7675b
Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
2026-01-22 14:11:54 +0700

    menambahkan data ke dalam tabel pengelolaan lewat pengelolaan seeder

--- manajemen-inventaris/database/seeders/pengelolaanSeeder.php
+++ manajemen-inventaris/database/seeders/pengelolaanSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class pengelolaanSeeder extends Seeder
 {
@@ -12,6 +13,26 @@ class pengelolaanSeeder extends Seeder
      */
     public function run(): void
     {
+        $persediaan = DB::table('persediaan')->get();
+        $peminjaman = DB::table('peminjaman')->get();
+
+
         
+        foreach($persediaan as $item_persediaan){
+                $pengelolaan = [
+                    'id_persediaan' => $item_persediaan->id,
+                    'kode_barang' => $item_persediaan->kode_barang,
+                    'nama_barang' => $item_persediaan->nama_barang,
+                    'masuk_stok' => $item_persediaan->jumlah,
+                ];
+                foreach($peminjaman as $item_peminjaman){
+                    $pengelolaan['id_peminjaman'] = $item_peminjaman->id;
+                    $pengelolaan['keluar_stok'] = $item_peminjaman->jumlah;
+                    $pengelolaan['total_stok'] = $pengelolaan['masuk_stok'] - $pengelolaan['keluar_stok'];
+                }
+                
+            }
+
+        DB::table('pengelolaan')->insert($pengelolaan);
     }
 }
